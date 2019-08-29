c499209d9621732121caed609cea374ef987bdf8
OlgaSavytska <o.savytska77@gmail.com>
2019-08-29 11:16:44 +0300

    Updates

---
+++ precache-manifest.0db10660878483cf06f934de75c0f8cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b9deadb9a7e0000cb053a8abb7814f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "56ff8710574f27cba1ed",
+    "url": "/goit-react-hw-01-components/static/css/main.3f8e5e27.chunk.css"
+  },
+  {
+    "revision": "aa5261ec0e0496c39469",
+    "url": "/goit-react-hw-01-components/static/js/2.9f671a27.chunk.js"
+  },
+  {
+    "revision": "56ff8710574f27cba1ed",
+    "url": "/goit-react-hw-01-components/static/js/main.5ff3c2c7.chunk.js"
+  },
+  {
+    "revision": "9d8ff468915371a5972d",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.bc6d281b.js"
+  }
+]);
